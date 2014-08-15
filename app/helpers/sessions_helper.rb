a683deae50c859fc829e3362efa6b7d6c0b6c499
Alec Bibat <alecbibat@gmail.com>
2014-08-15 12:21:07 -0600

    try to fix nomethoderror

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
 
-	def sign_in(user)
-		remember_token = User.new_remember_token
-		cookies.permanent[:remember_token] = remember_token
-		user.update_attribute(:remember_token, User.digest(remember_token))
-		self.current_user = user	
-	end
+	 def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
+  end
 
 	def signed_in?
 		!current_user.nil?
@@ -15,10 +15,10 @@ module SessionsHelper
 		@current_user = user
 	end
 
-	def current_user
-		remember_token = User.digest(cookies[:remember_token])
-		@current_user ||= User.find_by(remember_token: remember_token)
-	end
+	  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
 
 	def current_user?(user)
 		user == current_user
@@ -32,10 +32,11 @@ module SessionsHelper
 	end
 
 	def sign_out
-		current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
-		cookies.delete(:remember_token)
-		self.current_user = nil
-	end
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
 
 	def redirect_back_or(default)
 		redirect_to(session[:return_to] || default)
